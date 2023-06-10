611452ed7fc0251238c4e9a36510802c83877668
JoeTheorium <fernandoandresraggio@gmail.com>
2023-06-10 00:35:41 -0300

    Create component tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AddTaskButtonComponent } from './components/add-task-button/add-task-button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AddTaskButtonComponent
+    AddTaskButtonComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
